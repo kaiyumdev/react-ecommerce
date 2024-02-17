459d35026ec8dceffc90cadb68641000579860a8
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-17 23:50:41 +0600

    setup styles file & modify

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import "swiper/css";
+
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+// fonts and icons
+import "././assets/css/icofont.min.css";
+import "././assets/css/animate.css";
+import "././assets/css/style.min.css";
 
 const router = createBrowserRouter([
   {
