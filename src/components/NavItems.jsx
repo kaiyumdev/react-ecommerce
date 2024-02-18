6875edc6b55546532238bbab3a0fbe1ce6c27824
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-18 16:24:14 +0600

    header top section complete successfully

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [socialToggle, setSocialToggle] = useState(false);
+  const [headerFixed, setHeaderFixed] = useState(false);
+
+  //addEventListener
+  window.addEventListener("scroll", () => {
+    if (window.screenY > 200) {
+      setHeaderFixed(true);
+    } else {
+      setHeaderFixed(false);
+    }
+  });
+  return (
+    <header
+      className={`header-section style-4 ${
+        headerFixed ? "header-fixed fadeInUp" : ""
+      }  `}
+    >
+      {/* header top start */}
+      <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
+        <div className="container">
+          <div className="header-top-area">
+            <Link to="signUp" className="lab-btn me-3">
+              <span>Create Account</span>
+            </Link>
+            <Link to="login" className="">
+              Log In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default NavItems;
