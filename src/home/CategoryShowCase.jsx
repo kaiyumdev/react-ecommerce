d60d6b59d474995bcbb469688ca585c1525fe597
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-27 23:59:57 +0600

    updated categoryShowCase items & completed successfully

--- src/home/CategoryShowCase.jsx
+++ src/home/CategoryShowCase.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 
@@ -81,26 +81,27 @@ const ProductData = [
 
 const CategoryShowCase = () => {
   const [items, setItems] = useState(ProductData);
-
-  //category baded filtering
-  const filterItem = () => {};
+  const filterItem = (categItem) => {
+    const updateItems = ProductData.filter((curElem) => {
+      return curElem.cate === categItem;
+    });
+    setItems(updateItems);
+  };
   return (
     <div className="course-section style-3 padding-tb">
-      {/* shapes */}
       <div className="course-shape one">
-        <img src="/src/assets/images/shape-img/01.jpg" alt="" />
+        <img src="/src/assets/images/shape-img/icon/01.png" alt="education" />
       </div>
       <div className="course-shape two">
-        <img src="/src/assets/images/shape-img/02.png" alt="" />
+        <img src="/src/assets/images/shape-img/icon/02.png" alt="education" />
       </div>
-      {/* main section */}
       <div className="container">
         {/* section header */}
         <div className="section-header">
           <h2 className="title">{title}</h2>
           <div className="course-filter-group">
             <ul className="lab-ul">
-              <li onClick={() => filterItem("All")}>All</li>
+              <li onClick={() => setItems(ProductData)}>All</li>
               <li onClick={() => filterItem("Shoes")}>Shoes</li>
               <li onClick={() => filterItem("Bags")}>Bags</li>
               <li onClick={() => filterItem("Phones")}>Phones</li>
