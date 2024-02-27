b7867dd9d144afc22f460de2fc9c5a32a2cc29f6
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-27 17:58:30 +0600

    implements search products successfully

---
+++ src/components/SelectedCategory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectedCategory = (select) => {
+  return <select></select>;
+};
+
+export default SelectedCategory;
--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,21 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import productData from "../products.json";
+import { Link } from "react-router-dom";
+
+const title = (
+  <h2>
+    Search Your One From <span>Thousands</span> of Products
+  </h2>
+);
+const desc = "We have the largest collection of products";
+const bannerList = [
+  { iconName: "icofont-users-alt-4", text: "1.5 Million Customers" },
+  { iconName: "icofont-notification", text: "More then 2000 Marchent" },
+  { iconName: "icofont-globe", text: "Buy Anything Online" },
+];
 
 const Banner = () => {
-  const title = (
-    <h2>
-      Search Your One From <span>Thousands</span> of Products
-    </h2>
-  );
-  const desc = "We have the largest collection of products";
-  const bannerList = [
-    { iconName: "icofont-users-alt-4", text: "1.5 Million Customers" },
-    { iconName: "icofont-notification", text: "More then 2000 Marchent" },
-    { iconName: "icofont-globe", text: "Buy Anything Online" },
-  ];
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(productData);
+  console.log(productData);
+
+  //search functionality
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    setSearchInput(searchTerm);
+
+    //filtering products based on search
+    const filtered = productData.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
   return (
     <div className="banner-section style-4">
       <div className="container">
-        <div className="banner-content">{title}</div>
+        <div className="banner-content">
+          {title}
+          <form>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search your product"
+              value={searchInput}
+              onChange={handleSearch}
+            />
+            <button type="submit">
+              <i className="icofont-search"></i>
+            </button>
+          </form>
+          <p>{desc}</p>
+          <div className="lab-ul">
+            {searchInput &&
+              filteredProducts.map((product, i) => (
+                <li key={i}>
+                  <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                </li>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
