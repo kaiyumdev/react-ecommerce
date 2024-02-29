c371f4f5e322816d2c86f3b1d654f3a347da5f7e
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-29 19:01:56 +0600

    Complete footer section successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavItems from "./components/NavItems";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavItems></NavItems>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 }
