ecfb77b8d5144e0f75d24405e7625c229fdabbca
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-02 19:27:13 +0600

    Implements Tags successfully

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -7,6 +7,7 @@ import Pagination from "./Pagination";
 import Search from "./Search";
 import ShopCategory from "./ShopCategory";
 import PopularPost from "./PopularPost";
+import Tags from "./Tags";
 
 const showResults = "Showing 01 - 12 of 139 Results";
 
@@ -94,6 +95,7 @@ const Shop = () => {
                   selectedCategory={selectedCategory}
                 ></ShopCategory>
                 <PopularPost></PopularPost>
+                <Tags></Tags>
               </aside>
             </div>
           </div>
