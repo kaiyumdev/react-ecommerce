592e975025cd9122ed5e36231c57d9db138eface
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-06 20:19:45 +0600

    Implements css & bootstrap file for styles successfully

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "././assets/css/icofont.min.css";
 import "././assets/css/animate.css";
 import "././assets/css/style.min.css";
 
+//routes
 const router = createBrowserRouter([
   {
     path: "/",
