2912710911ca0d57a7f000b6f6aef3173b226511
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-10 17:57:21 +0600

    Updated e-commerce project & solve image error successfully

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -10,19 +10,19 @@ const expareance = "Years Of Experiences";
 
 const aboutList = [
   {
-    imgUrl: "/src/assets/images/about/icon/01.jpg",
+    imgUrl: "/images/about/icon/01.jpg",
     imgAlt: "about icon rajibraj91 rajibraj",
     title: "Skilled Instructors",
     desc: "Distinctively provide acces mutfuncto users whereas communicate leveraged services",
   },
   {
-    imgUrl: "/src/assets/images/about/icon/02.jpg",
+    imgUrl: "/images/about/icon/02.jpg",
     imgAlt: "about icon rajibraj91 rajibraj",
     title: "Get Certificate",
     desc: "Distinctively provide acces mutfuncto users whereas communicate leveraged services",
   },
   {
-    imgUrl: "/src/assets/images/about/icon/03.jpg",
+    imgUrl: "/images/about/icon/03.jpg",
     imgAlt: "about icon rajibraj91 rajibraj",
     title: "Online Classes",
     desc: "Distinctively provide acces mutfuncto users whereas communicate leveraged services",
