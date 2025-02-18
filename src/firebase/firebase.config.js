c727481a061a8664205adddb74ff952a3456a951
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-19 00:17:26 +0600

    feat(firebaseConfig): updated firebaseConfig file successfully

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,7 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-console.log("API Key:", import.meta.env.VITE_apiKey);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
